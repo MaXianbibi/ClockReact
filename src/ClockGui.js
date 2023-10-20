ac1f7f690fa21d964b891b5817194ec415cc6e06
Justin Morneau <jmorneau25@gmail.com>
2023-10-20 15:27:45 +0900

    finish

--- src/ClockGui.js
+++ src/ClockGui.js
@@ -1,23 +1,23 @@
 import { useLightMode } from "./LightMode";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function GuiInterface({children}) {
-    // 1. Initialisez un state pour la valeur choisie
-    const [selectedSecond, setselectedSecond] = useState('');
+const SECONDS_IN_A_MINUTE = 60;
+const secondsArray = Array.from({ length: SECONDS_IN_A_MINUTE }, (_, index) => index);
 
-    // 2. Gestionnaire d'événement pour écouter les modifications de la sélection
+function GuiInterface({ label, value, onChange }) {
     const handleSelectionChange = (event) => {
-        setselectedSecond(event.target.value);
+        const selectedValue = event.target.value;
+        onChange(selectedValue);
     };
 
     return (
-        <div style={{width: "100%"}}>
-            <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap", width: "100%"}}>
-                <label htmlFor="second">{children}</label>
-                <select value={selectedSecond} onChange={handleSelectionChange}>
-                    {[...Array(60).keys()].map((_, index) => (
-                        <option key={index} value={index}>
-                            {index}
+        <div style={{ width: "100%" }}>
+            <div style={{ display: "flex", justifyContent: "space-around", flexWrap: "wrap", width: "100%" }}>
+                <label htmlFor="second">{label}</label>
+                <select value={value} onChange={handleSelectionChange}>
+                    {secondsArray.map((value) => (
+                        <option key={value} value={value}>
+                            {value}
                         </option>
                     ))}
                 </select>
@@ -26,18 +26,48 @@ function GuiInterface({children}) {
     );
 }
 
+
+function Clock({ seconds, minutes, hours, lightMode }) {
+    const clockStyle = {
+        backgroundColor: lightMode ? '#000' : '#fff',
+        color: lightMode ? '#fff' : '#000',
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: '100px',
+        height: "auto",
+        width: "auto",
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
+
+    return (
+        <div style={clockStyle}>
+            <p style={{ margin: "0" }}>{hours.toString().padStart(2, '0')}:{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</p>
+        </div>
+    );
+}
+
 export function ClockGui() {
     const { lightMode } = useLightMode();
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+
+
+    const [isRunning, setIsRunning] = useState(false);
+
 
     const clockStyle = {
         backgroundColor: lightMode ? '#000' : '#fff',
         color: lightMode ? '#fff' : '#000',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '50%',
         width: '50%',
     };
+
     const bodyStyle = {
         height: '100%',
         width: '100%',
@@ -56,26 +86,78 @@ export function ClockGui() {
         justifyContent: 'center',
         width: "15%",
         height: "50%"
-
     }
+
+    const handleSecondChange = (value) => {
+        setSeconds(prev => value);
+        console.log("Selected second:", value);
+    };
+
+    const handleMinuteChange = (value) => {
+        setMinutes(prev => value);
+        console.log("Selected minute:", value);
+    };
+
+    const handleHourChange = (value) => {
+        setHours(prev => value);
+        console.log("Selected hour:", value);
+    };
+
+    useEffect(() => {
+        let intervalId;
+    
+        if (isRunning) {
+            intervalId = setInterval(() => {
+                
+                if (seconds > 0) {
+                    setSeconds(seconds - 1);
+                } else if (minutes > 0) {
+                    setSeconds(59);
+                    setMinutes(minutes - 1);
+                } else if (hours > 0) {
+                    setSeconds(59);
+                    setMinutes(59);
+                    setHours(hours - 1);
+                } else {
+                    setIsRunning(false);
+                }
+    
+            }, 1000);
+        }
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [isRunning, seconds, minutes, hours]);
+    
+    
+    
+
 
     return (
         <div style={bodyStyle}>
-
             <div style={guiStyle}>
-                <GuiInterface>
-                    Seconds :
-                </GuiInterface>
-                <GuiInterface>
-                    Minutes :
-                </GuiInterface>
-                <GuiInterface>
-                    Hours :
-                </GuiInterface>
+                <GuiInterface
+                    label="Seconds :"
+                    onChange={handleSecondChange}
+                />
+                <GuiInterface
+                    label="Minutes :"
+                    onChange={handleMinuteChange}
+                />
+                <GuiInterface
+                    label="Hours :"
+                    onChange={handleHourChange}
+                />
             </div>
-
-            <div style={clockStyle}></div>
-
+            <div style={clockStyle}>
+                <Clock
+                    seconds={seconds}
+                    minutes={minutes}
+                    hours={hours}
+                    lightMode={lightMode}
+                />
+                <button onClick={() => { setIsRunning(prev => !prev) }}>{isRunning ? "Stop" : "Start"}</button>
+            </div>
         </div>
     );
 }
